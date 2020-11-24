84034c313228770d8471e2c8f880c5faada4b68f
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-11-24 12:24:51 +0530

    Random Description added

--- music.py
+++ music.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Music(BaseModel):
-    acousticness: float
-    danceability: float
-    energy: float
-    instrumentalness: float
-    liveness: float
-    speechiness: float
-    tempo: float
-    valence: float
+    acousticness: float = Field(..., description="Here is the description of the input to be sent")
+    danceability: float = Field(..., description="Here is the description of the input to be sent")
+    energy: float = Field(..., description="Here is the description of the input to be sent")
+    instrumentalness: float = Field(..., description="Here is the description of the input to be sent")
+    liveness: float = Field(..., description="Here is the description of the input to be sent")
+    speechiness: float = Field(..., description="Here is the description of the input to be sent")
+    tempo: float = Field(..., description="Here is the description of the input to be sent")
+    valence: float = Field(..., description="Here is the description of the input to be sent")
